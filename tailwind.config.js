361b8c7c6a6373fe8bd14ee36c6ded04921ddb26
jorgeProgramaEn5 <jorgitoestrada2924@gmail.com>
2023-06-29 14:14:01 -0500

    add styles navbar-3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,41 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      keyframes: {
-        wiggle: {
-          '0%': { transform: 'translateY(100px)' },
-          '100%': { transform: 'translateY(0px)' },
-        }
-      },
-      animation: {
-        wiggle: 'wiggle 0.5s ease-in-out',
-      },
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-      colors: {
-        primary:{
-          100:"#7958d7",
-          200:"#63bff3",
-          300:"#5ae3b1",
-          400:"#ffcc41"
-        },
-        secondary:{
-          100:"#ff7c3e",
-          200:"#63bff3",
-          300:"#5ae3b1",
-          400:"#ffcc41"
-        }
-      }
-    },
+    extend: {},
   },
   plugins: [],
 }
+
