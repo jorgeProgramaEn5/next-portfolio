3cc95a6339ff50516f83e6326d961c4f8dbfce1d
jorgeProgramaEn5 <jorgitoestrada2924@gmail.com>
2023-07-02 08:13:26 -0500

    add skills-styles-2

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+'use client'
+import React from 'react';
+import { useState} from 'react';
+
 
 
 
 export function Skills() {
+  const [view, setView] = useState('hidden');
+  const [botonText, setBotonText] = useState('Read more');
+
+  const button = () => {
+    if (view === 'hidden') {
+      setView('')
+      setBotonText('Read less')
+    } else {
+      setView('hidden')
+      setBotonText('Read more')
+    }
+  }
+
   return (
     <section>
         <div className="mx-auto flex w-[85%] md:w-[85%] lg:w-[90%] flex-col justify-center bg-white rounded-2xl shadow-xl shadow-gray-400/20">
@@ -13,7 +29,7 @@ export function Skills() {
                   I am a front-end developer with experience in various tools and technologies. 
                   My specialization lies in Next.js, React, and Figma technologies, although 
                   I also have experience in other essential tools such as HTML, CSS, JavaScript, 
-                  Git, Bootstrap, and Tailwind. <span className=' hidden md:block '>
+                  Git, Bootstrap, and Tailwind. <span className={`${view}`}>
                   I handle these tools with ease and use them to create attractive, responsive, 
                   and functional websites and applications. As a developer, my goal is always to 
                   provide high-quality solutions that exceed the expectations of my clients and 
@@ -21,13 +37,13 @@ export function Skills() {
                   </span>
                 </p>
 
-              <div className="mt-10 flex items-center justify-center gap-x-6 lg:justify-start">
-              <a
-                href="#"
-                className="rounded-md bg-white px-3.5 py-2.5 text-sm md:text-base lg:text-lg font-semibold text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
+              <div className=" md:hidden mt-10 flex items-center justify-center gap-x-6 lg:justify-start">
+              <div
+                onClick={()=> button()}
+                className="rounded-md bg-white px-3.5 py-2.5 text-sm md:text-base lg:text-lg font-semibold text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white cursor-pointer"
               >
-                Get started
-              </a>
+                {botonText}
+              </div>
             </div>
             </div>
         </div>
