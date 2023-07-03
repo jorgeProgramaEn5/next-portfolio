f084b89760ef6a2d7f1b32f55077858b01edcd97
jorgeProgramaEn5 <jorgitoestrada2924@gmail.com>
2023-07-02 20:24:11 -0500

    add contact

---
+++ src/Components/ContactEmailjs.jsx
@@ -0,0 +1,71 @@
+'use client'
+import { contact_img } from '@/Constant/indice';
+import React, { useRef, useState } from 'react';
+import emailjs from '@emailjs/browser';
+
+
+
+export function Contact() {
+
+    const form = useRef();
+    const [buttonText, setButtonText] = useState("send");
+    const [buttonTextDesable, setButtonTextDesable] = useState("text-enable");
+    const [buttonTextEnable, setButtonTextEnable] = useState("text-desable");
+
+
+    const sendEmail = (e) => {
+      e.preventDefault();
+  
+      emailjs.sendForm('service_m6g1ufi', 'template_bupwt18', form.current, 'PriAIdQ5OpabYNxw0')
+        .then((result) => {
+            console.log(result.text);
+            setButtonText("Message sent successfully")
+            setButtonTextDesable("text-desable")
+            setButtonTextEnable("text-enable")
+        }, (error) => {
+            console.log(error.text);
+            setButtonText("Ups! Something went wrong")
+            setButtonTextDesable("text-desable")
+            setButtonTextEnable("text-enable")
+        });
+    };
+  
+    return (
+      <section className='contact' id='contact'>
+              <div className='aling-items-center'>
+                  <div md={6} className='imgContact'>
+                      <img src={contact_img} alt="contact" />
+                  </div>
+
+                    <div md={6} className='contact-form'>
+                        <h2>Get In Touch</h2>
+                        <form ref={form} onSubmit={sendEmail}>
+                            <div>
+                                <div sm={6} xs={6} className='px-1'>
+                                    <input type="text" name="from_name"  placeholder='First Name' required/>
+                                </div>
+    
+                                <div sm={6} xs={6} className='px-1'>
+                                    <input type="text" name="from_lastname" placeholder='Last Name'/>
+                                </div>
+    
+                                <div sm={6} xs={6} className='px-1'>
+                                    <input type="email" name="from_email" placeholder='Email Address' required/>
+                                </div>
+    
+                                <div sm={6} xs={6} className='px-1'>
+                                    <input type="tel" name="phone" placeholder='Phone Number' required/>
+                                </div>
+    
+                                <div className='col-text-area'>
+                                    <textarea style={{ resize: "none" }} name="message" row="6" placeholder='Message' required></textarea>
+                                    <input type="submit" value={buttonText} className={buttonTextDesable} onClick={()=>setButtonText("Sending...")}/>
+                                    <input type="submit" value={buttonText} className={buttonTextEnable} disabled/>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+              </div>
+      </section>
+    )
+  }
--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -34,7 +34,7 @@ export function Projects() {
 
     return (
     <section className='' id='projects'>
-        <div className='  flex flex-col justify-center items-center pt-7' id='projects'>
+        <div className='  flex flex-col justify-center items-center pt-7 mt-7' id='projects'>
             <div className="inline-flex rounded-md shadow-sm" >
                 <button onClick={()=> clickStep1()} type="button" className="px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-l-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white">
                     <span>All</span>
